7a415c89b2da79a7b1731232647df82cfabdcf9a
Max <mushbit@protonmail.com>
2023-03-07 16:03:29 +0100

    Reworked the Graph#draw method to be read better. Basically started from scratch but with a better idea as of how to go about it. Initial core plan has not changed.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,42 +1,45 @@
 require 'pry-byebug'
 # Used to manage graph
 class Graph
-  LINE_ROW = ' ___________'
-  SANDWICH_ROW = '    |   |   '
   @@rows = {
-    x: '  1   2   3 ',
-    sandwich_row1: SANDWICH_ROW,
-    a: ['A', 'X', 'X', 'X'],
-    line_row2: LINE_ROW,
-    sandwich_row2: SANDWICH_ROW,
-    b: ['B', 'X', 'O', 'O'],
-    line_row3: LINE_ROW,
-    sandwich_row3: SANDWICH_ROW,
-    c: ['C', ' ', ' ', ' ']
+    a: [' ', ' ', ' '],
+    b: [' ', ' ', ' '],
+    c: [' ', ' ', ' '],
+    one: [' ', ' ', ' '],
+    two: [' ', ' ', ' '],
+    three: [' ', ' ', ' '],
+    tr_to_bl: [' ', ' ', ' '],
+    tl_to_br: [' ', ' ', ' ']
   }
 
   def self.draw
-    @@rows.each do |key, row|
-      graph = ''
+    line_row = ' ___________'
+    sandwich_row = '    |   |   '
+    graph = "\n      1   2   3
+    #{sandwich_row}
+    A #{@@rows[:a].join(' | ')}
+    #{line_row}
+    #{sandwich_row}
+    B #{@@rows[:b].join(' | ')}
+    #{line_row}
+    #{sandwich_row}
+    c #{@@rows[:c].join(' | ')}
+    \n"
+    puts graph
+  end
 
-      graph += if key.match(/^a|^b|^c/m)
-                 "#{row[0]} #{row[1]} | #{row[2]} | #{row[3]} "
-               else
-                 row
-               end
-      puts graph
-    end
+  def self.update_mark_placement
   end
 
   def self.check_win_condition
     @@rows.each do |key, row|
       next if key.match(/^x|sandwich_row|line_row/)
 
-      # Checks horizontal condition to win, being 3 in a row
-      if row.tally['X'] === 3
-        puts '--- X wins! ---'
-      elsif row.tally['O'] === 3
-        puts '--- O wins! ---'
+      case
+      when row.tally['X'] == 3
+        puts '--- X wins this round! ---'
+      when row.tally['O'] == 3
+        puts '--- O wins this round! ---'
       end
     end
   end
@@ -45,7 +48,7 @@ end
 # Super class for marking behavior
 class Piece < Graph
   def mark(row, column)
-    @@rows[row.to_sym][column] = @marking
+    @@rows[row.downcase.to_sym][column - 1] = @marking
     Graph.draw
   end
 end
