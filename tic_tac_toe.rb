ee4d2d6906fc8437405527c088bc68468be918cd
Max <mushbit@protonmail.com>
2023-03-10 11:04:37 +0100

    stopped class Graph from being a super class, and rubocop seems to be content once more!! Instead of using a class variable to log player markings I use a class instance seperate from the player. When I want to reset the graph, I re-construct and tadaaa!! Happy days :D Next: create ways to check winning conditions without using literals, in order to decrease bulk (and appease rubocop!)

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,64 +1,57 @@
 require 'pry-byebug'
 # Used to manage graph
 class Graph
-  def track_mark_placement
-    @@rows = {
-      a: ['O', 'O', ' '],
+  attr_accessor :rows
+
+  def initialize
+    @rows = {
+      a: [' ', ' ', ' '],
       b: [' ', ' ', ' '],
       c: [' ', ' ', ' ']
     }
   end
 
-  def self.draw
-    line_row = ' ___________'
-    sandwich_row = '    |   |   '
-    graph = "\n      1   2   3
-    #{sandwich_row}
-    A #{@@rows[:a].join(' | ')}
-    #{line_row}
-    #{sandwich_row}
-    B #{@@rows[:b].join(' | ')}
-    #{line_row}
-    #{sandwich_row}
-    c #{@@rows[:c].join(' | ')}
-    \n"
-    puts graph
-  end
-
-  def self.reset
-    @@rows = {
-      a: ['O', 'O', ' '],
-      b: [' ', ' ', ' '],
-      c: [' ', ' ', ' ']
-    }
+  class << self
+    def draw
+      line_row = ' ___________'
+      sandwich_row = '    |   |   '
+      graph = "\n      1   2   3
+      #{sandwich_row}
+      A #{rows[:a].join(' | ')}
+      #{line_row}
+      #{sandwich_row}
+      B #{rows[:b].join(' | ')}
+      #{line_row}
+      #{sandwich_row}
+      c #{rows[:c].join(' | ')}
+      \n"
+      puts graph
+    end
   end
 end
 
 # Super class for marking X's and O's
-class Player < Graph
-  attr_reader :name
-
+class Player
   def initialize(name)
-    super
     @name = name
   end
 
   def mark(row, column)
-    @@rows[row.downcase.to_sym][column - 1] = @marking
+    game.rows[row.downcase.to_sym][column - 1] = @marking
     Graph.draw
     check_win
   end
 
   def check_win
     stats = [
-      @@rows[:a],
-      @@rows[:b],
-      @@rows[:c],
-      [@@rows[:a][0], @@rows[:b][0], @@rows[:c][0]],
-      [@@rows[:a][1], @@rows[:b][1], @@rows[:c][1]],
-      [@@rows[:a][2], @@rows[:b][2], @@rows[:c][2]],
-      [@@rows[:a][0], @@rows[:b][1], @@rows[:c][2]],
-      [@@rows[:c][0], @@rows[:b][1], @@rows[:a][2]]
+      game.rows[:a],
+      game.rows[:b],
+      game.rows[:c],
+      [game.rows[:a][0], game.rows[:b][0], game.rows[:c][0]],
+      [game.rows[:a][1], game.rows[:b][1], game.rows[:c][1]],
+      [game.rows[:a][2], game.rows[:b][2], game.rows[:c][2]],
+      [game.rows[:a][0], game.rows[:b][1], game.rows[:c][2]],
+      [game.rows[:c][0], game.rows[:b][1], game.rows[:a][2]]
     ]
     stats.each do |score|
       break unless score.tally[@marking] == 3
