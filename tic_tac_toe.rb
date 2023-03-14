3ad21202d7018c1dbcb6a9a43b38877c4af5d631
Max <mushbit@protonmail.com>
2023-03-14 16:33:58 +0100

    I stepped away from having the marking values placed in a hash of arrays and now work with an array of arrays. This simplified the #check_..._score. There was a little issue with Array.new(3. Array.new(3, " ")). After some hairpulling I figured it out and ended up appreciating how powerfull of a tool this can be, alas, I am merely working with tic-tac toe :) Next: Have classPlayer work with classGraph. Add classGame to add interaction to.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,42 +1,46 @@
 require 'pry-byebug'
 # Used to manage graph
 class Graph
-  attr_accessor :rows
+  attr_accessor :tiles
 
   def initialize
-    @rows = {
-      a: [' ', ' ', ' '],
-      b: ['X', ' ', 'O'],
-      c: [' ', 'X', 'X']
-    }
+    @tiles = Array.new(3) { Array.new(3, ' ') }
   end
 
   def draw
     line_row = ' ---+---+---'
-    graph = "\n      1   2   3
-    A #{rows[:a].join(' | ')}
+    board = "\n      1   2   3
+    A #{tiles[0].join(' | ')}
     #{line_row}
-    B #{rows[:b].join(' | ')}
+    B #{tiles[1].join(' | ')}
     #{line_row}
-    c #{rows[:c].join(' | ')}
+    c #{tiles[2].join(' | ')}
     \n"
-    puts graph
+    puts board
   end
 
-  def get_stats
-    abc = [:a, :b, :c]
-    stats = [
-      rows.map { |_k, v| v },
-      (0..2).map { |i| rows.map { |_k, v| v[i] } },
-      [[rows[:a][0], rows[:b][1], rows[:c][2]]],
-      [[rows[:c][0], rows[:b][1], rows[:a][2]]]
-    ].flatten(1)
+  def check_horizontal_score
+    tiles.map { |v| v }
+  end
+
+  def check_vertical_score
+    (0..2).map { |i| tiles.map { |v| v[i] } }
+  end
+
+  def check_diagonal_score
+    [0, 2].map do |y|
+      case y
+      when 0
+        (0..2).map { |i| tiles[0 + i][y + i] }
+      when 2
+        (0..2).map { |i| tiles[0 + i][y - i] }
+      end
+    end
   end
 
   def mark(row, column, marking)
-    rows[row.downcase.to_sym][column - 1] = marking
-    Graph.draw
-    check_win
+    tiles[row - 1][column - 1] = marking
+    draw
   end
 end
 
@@ -49,6 +53,7 @@ class Player
   def choose_location(row, column)
     game.mark(row, column, @marking)
   end
+
   def check_win
     stats.each do |score|
       break unless score.tally[@marking] == 3
@@ -57,7 +62,6 @@ class Player
       @wins += 1
 
       puts "--- #{@name} won the game! ---" if @wins == 3
-
     end
   end
 end
@@ -79,3 +83,16 @@ class Cross < Player
     @wins = 0
   end
 end
+
+game = Graph.new
+player1 = Cross.new('John')
+player2 = Circle.new('Frank')
+game.mark(1, 1, 'X')
+game.mark(3, 1, 'X')
+game.mark(2, 2, 'X')
+game.mark(1, 2, 'O')
+game.mark(1, 3, 'O')
+game.mark(3, 3, 'O')
+p game.check_diagonal_score
+p game.check_horizontal_score
+p game.check_vertical_score
